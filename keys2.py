8b7271449b0f4c8fdcc1f97ef0c51bb85655d54b
MohammedEsamaldin <mohammedesam.mem@gmail.com>
2024-01-31 18:26:42 +0200

    last edits

---
+++ keys2.py
@@ -0,0 +1,20 @@
+# Mohammed personal user 
+api_key  = 'v^1.1v^1.1#i^1#I^3#f^0#r^0#p^1#t^H4sIAAAAAAAAAOVYa2wUVRTebbcYXgIKiA3iMkgk1Jm9Mzvd3Zm0K0sfdIE+d2mgseLdmTt07M7DuXfbbkCtNWIMKokCJRGxGOQHRiQxPiKGAMrDBLWWRn6IETVNSBQiqOGHGp3ZlrKtBJBuYhP3z2bOPffc7/vOOffeGdA5YeLijVUbL09135bX0wk689xudjKYOKGg6Pb8vMICF8hycPd03tfp6co/V4KhljTFBoRNQ8fI26EldSxmjKVUytJFA2IVizrUEBaJJMYi1StFjgGiaRnEkIwk5Y2Wl1IyBBKAnOAHIVaAcsC26ldixo1SSpJgIiFAXgoGQrCYlexxjFMoqmMCdVJKcYDz0yxHc8E4GxT5YhHwTFDgmihvI7Kwaui2CwOocAaumJlrZWG9PlSIMbKIHYQKRyOVsdpItLyiJl7iy4oVHtIhRiBJ4ZFPZYaMvI0wmULXXwZnvMVYSpIQxpQvPLjCyKBi5AqYW4CfkRqGECcE+FBCQLbSkM+JlJWGpUFyfRyORZVpJeMqIp2oJH0jRW01Eo8iiQw91dghouVe568+BZOqoiKrlKpYGlkTqaujwtVGC9Q0JNP1bRqWLGjSdQ3ltKLwELEI8DSUBQXBBD+00GC0IZlHrVRm6LLqiIa9NQZZimzUaLQ2/ixtbKdavdaKKMRBlO0nXNEwFGpykjqYxRRp0Z28Is0Wwpt5vHEGhmcTYqmJFEHDEUYPZCSyc22aqkyNHszU4lD5dOBSqoUQU/T52tvbmXY/Y1jrfBwArG919cqY1II0SNm+Tq8P+qs3nkCrGSoSsmdiVSRp08bSYdeqDUBfR4X5UIDnAkO6j4QVHm39hyGLs29kR+SqQ1BAFgCQlSAXEngAc9Eg4aEa9TkwUAKmaQ1arYiYSSghWrLLLKUhS5XtmlI4f0hBtBwQFJoXFIVOFMsBmlUQAgglEpIQ+j/1yc1WegxJFiI5KfWclXk1H9Fq9LL61Q31wRAUJElviHGSJjRGk8tXWyurm1Y8pixN68EatbX0ZpvhmuTLkqqtTNxef/z1epWBCZLHRC8mGSaqM5KqlB5fCfZbch20SDqGkknbMCaSEdOM5marzl39/rtt4tZ45+6I+o+Op2uywk7Jji9WznxsB4CmyjgnECMZms9weh3atw/HvDaDeky8VfviOq5Y2yQH2ary4I2TMRy6DG6TGAthI2XZl22m1rmAxY1WpNvnGbGMZBJZjeyY+1nTUgQmkmi8NXYOClyF4+ywZYPAHxAEFoxtO5IyR+na8bYl5WQr9lTe2q3aN/IdP+zK/Ngu9xHQ5T6Y53aDErCQXQDmT8hf5cmfUohVghgVKgxW1+n2q6uFmFaUNqFq5d3puvT61qqyworabYvXx9O9rxx3Tcn6xNDTDOYMf2SYmM9OzvriAOZeHSlgp901lfOzHBdkg3wx4JvAgqujHna2Z+Zv3Rc2F9UtCT4xKbZ8C+ca+OiTZ18GU4ed3O4Cl6fL7aLfnb678JfTjzz9prTorXOeI5E3jvVYl59f9WH3jK79p3wXf7UWnpi/uXugufeLlr3FoX2H53166OEXN/bu0+OLLoabt+8+dOkD/93vHGnlm+t2ndq+qXPGUWbeyc+nfLd1YFHf2bOzLvRXln8fmrlhWeLHNfOkY/sXC5We56rS/TI3p7e1ApT/caZo8p7OknuPEu+ytr79fRu621594Mv3z/98elZ40rcPPfjnip3rJ5j3p06WuU4tt/bg4zv71p7Y2+U58M2mg+dDcNfJ6dufPNxXfs8d4YqPP+tvfaHIte1t95kDTz2zrLl/9l87Ylu482cgOhQt2NUxTft67sBP5177yvTEH9+x5D3ye/sPLw3m8m/uXbl5/BEAAA=='
+app_id_1 = 'Mohammed-Qvmscrap-PRD-ff4ae1e04-ad9feab4'
+client_secre_1 = 'PRD-f4ae1e04486a-3a14-4949-afae-5878'
+
+
+# Q Parts user (dataprojectsqvm@gmail.com)
+app_id_2 = 'Qvmparts-Productc-PRD-1e584dc36-9bda5d05'
+client_secre_2 = 'PRD-e584dc360355-715d-45b6-aad1-25ea'
+refresher_token =  "v^1.1#i^1#f^0#I^3#r^1#p^3#t^Ul4xMF8xMTo2N0MyNkRGRUU1QkQyQURDNDgxNDJBQTNGNTk2MUE1Rl8wXzEjRV4yNjA="
+
+# Duaa user
+app_id_3 = 'marocoma-Duaadeal-PRD-ae5603d2f-872cef27'
+client_secre_3 = 'PRD-e5603d2f1627-ecd9-4172-bd12-339b'
+refresher_token_3 = 'v^1.1#i^1#r^1#f^0#I^3#p^3#t^Ul4xMF8wOjM4QkMwQTZFNDc0MkJBMUE0MTA1RTQ4OUM1ODgzNkQxXzBfMSNFXjI2MA=='
+
+# Sara user
+app_id_4 = 'JerryJoy-saradeal-PRD-ce5603d2f-eb9b4f3a'
+client_secre_4 = 'PRD-e5603d2fc9dd-ef7c-4e07-8ee0-d3e4'
+refresher_token_4 = 'v^1.1#i^1#p^3#f^0#r^1#I^3#t^Ul4xMF8xMTo1NkNBQ0U2NzQyMzdENkVFRjlCRTQ0NTVEREVGRTlFNF8wXzEjRV4yNjA='
